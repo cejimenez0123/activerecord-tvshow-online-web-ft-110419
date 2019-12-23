9daef9f2180b5f19c1bcdc701924e104a9bf1137
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-23 19:08:45 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,4 +11,7 @@ class Show < ActiveRecord::Base
   def self.least_popular_show
     self.where("rating = ?",self.lowest_rating).first
   end
+  def self.ratings_sum
+    self.sum(:rating)
+  end
 end
