06d14a99d82ff2289642cab8a145e32b8113c3d5
Christian Jimenez <christianjimenez0123@gmail.com>
2019-12-23 19:06:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,7 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = #{self.highest_rating}").first
   end
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 end
